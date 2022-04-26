363d50994e0cdb7894641a10485a209005835be1
Kirill <nemollirik@gmail.com>
2022-04-26 14:00:09 +0500

    REST-API доделано

--- rest_api/pupils_resources.py
+++ rest_api/pupils_resources.py
@@ -1,31 +1,19 @@
 from flask_restful import reqparse, abort, Resource
-from sqlalchemy.exc import IntegrityError
 from data import db_session
 from data.users import User
 from data.pupils import Student
+from data.staff import Employee
 from flask import jsonify
 from datetime import date
-from modules import key
-
-
-def check_api_key():
-    add_args = put_parser.parse_args()
-    if not key.check_key(add_args['key']):
-        abort(404, message=f"Error: No valid API key provided")
-        return False
-    return True
 
 
 def abort_if_student_not_found(student_id):
-    employee = db_session.create_session().query(Student).get(student_id)
-    if not employee:
+    if not db_session.create_session().query(Student).get(student_id):
         abort(404, message=f"Student {student_id} not found")
 
 
 class PupilsResource(Resource):
     def get(self, student_id):
-        if not check_api_key():
-            return
         abort_if_student_not_found(student_id)
         return jsonify(
             {'student': db_session.create_session().query(Student).get(student_id).to_dict(only=(
@@ -33,80 +21,66 @@ class PupilsResource(Resource):
                 'date_of_birth'))})
 
     def delete(self, student_id):
-        if not check_api_key():
-            return
         abort_if_student_not_found(student_id)
         db_sess = db_session.create_session()
-        user = db_sess.query(Student).get(student_id).user
-        db_sess.delete(user)
-        db_sess.delete(db_sess.query(Student).get(student_id))
+        student = db_sess.query(Student).get(student_id)
+        db_sess.delete(student.user)
+        db_sess.delete(student)
         db_sess.commit()
         return jsonify({'success': 'OK'})
 
     def put(self, student_id):
-        if not check_api_key():
-            return
         args = put_parser.parse_args()
         abort_if_student_not_found(student_id)
         db_sess = db_session.create_session()
         student = db_sess.query(Student).get(student_id)
-        user = db_sess.query(Student).get(student_id).user
-        if 'surname' in args:
-            student.surname = args['surname']
-            user.surname = args['surname']
-        if 'name' in args:
-            student.name = args['name']
-            user.name = args['name']
-        if 'patronymic' in args:
-            student.patronymic = args['patronymic']
-            user.patronymic = args['patronymic']
-        if 'email' in args:
-            student.email = args['email']
-            user.email = args['email']
-        if 'address' in args:
+        user = student.user
+        if args['surname']:
+            student.surname = user.surname = args['surname']
+        if args['name']:
+            student.name = user.name = args['name']
+        if args['patronymic']:
+            student.patronymic = user.patronymic = args['patronymic']
+        if args['email']:
+            if db_sess.query(User).filter(User.email == args['email']).first():
+                return jsonify({'Error': 'A user with this email already exists.'})
+            student.email = user.email = args['email']
+        if args['address']:
             student.address = args['address']
-        if 'native_city' in args:
-            student.native_city = args['native_city']
-        if 'class_' in args:
+        if args['class_']:
+            if not db_sess.query(Employee).filter(Employee.class_ == args["class_"]).first():
+                return jsonify({'Error': 'A class teacher has not yet been appointed for this class.'})
             student.class_ = args['class_']
-        if 'password' in args:
+        if args['password']:
             user.set_password(args['password'])
-        try:
-            db_sess.commit()
-        except IntegrityError:
-            db_sess.rollback()
-            return jsonify({'error': 'IntegrityError'})
+        db_sess.commit()
         return jsonify({'success': 'OK'})
 
 
 class PupilsListResource(Resource):
     def get(self):
-        if not check_api_key():
-            return
         return jsonify({'pupils': [student.to_dict(only=(
             'id', 'surname', 'name', 'patronymic', 'age', 'class_', 'address', 'email', 'native_city', 'date_of_birth'))
             for
             student in db_session.create_session().query(Student).all()]})
 
     def post(self):
-        if not check_api_key():
-            return
         args = parser.parse_args()
         db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == args['email']).first():
+            return jsonify({'Error': 'A user with this email already exists.'})
+        if not db_sess.query(Employee).filter(Employee.class_ == args["class_"]).first():
+            return jsonify({'Error': 'A class teacher has not yet been appointed for this class.'})
         student = Student(surname=args['surname'], name=args['name'], patronymic=args['patronymic'],
                           address=args['address'], email=args['email'], native_city=args['native_city'],
                           class_=args['class_'])
         user = User(surname=args['surname'], name=args['name'], patronymic=args['patronymic'], email=args['email'])
-        student.set_date(args['date_of_birth'])
+        student.set_date(date(*map(int, args["date_of_birth"].split("-"))))
         user.set_password(args['password'])
-        user.set_date(args['date_of_birth'])
+        user.set_date(date(*map(int, args["date_of_birth"].split("-"))))
         db_sess.add(student)
         db_sess.add(user)
-        try:
-            db_sess.commit()
-        except IntegrityError:
-            db_sess.rollback()
-            return jsonify({'error': 'IntegrityError'})
+        db_sess.commit()
         return jsonify({'success': 'OK'})
 
 
@@ -117,16 +91,15 @@ parser.add_argument('patronymic', required=True)
 parser.add_argument('address', required=True)
 parser.add_argument('native_city', required=True)
 parser.add_argument('class_', required=True)
-parser.add_argument('date_of_birth', required=True, type=date)
+parser.add_argument('date_of_birth', required=True)
 parser.add_argument('email', required=True)
 parser.add_argument('password', required=True)
 put_parser = reqparse.RequestParser()
-parser.add_argument('surname')
-parser.add_argument('name')
-parser.add_argument('patronymic')
-parser.add_argument('address')
-parser.add_argument('native_city')
-parser.add_argument('class_')
-parser.add_argument('email')
-parser.add_argument('password')
-put_parser.add_argument('key', required=True)
+put_parser.add_argument('surname')
+put_parser.add_argument('name')
+put_parser.add_argument('patronymic')
+put_parser.add_argument('address')
+put_parser.add_argument('native_city')
+put_parser.add_argument('class_')
+put_parser.add_argument('email')
+put_parser.add_argument('password')
--- rest_api/users_resources.py
+++ rest_api/users_resources.py
@@ -3,34 +3,20 @@ from data import db_session
 from data.users import User
 from flask import jsonify
 from datetime import date
-from modules import key
-
-
-def check_api_key():
-    add_args = put_parser.parse_args()
-    if not key.check_key(add_args['key']):
-        abort(404, message=f"Error: No valid API key provided")
-        return False
-    return True
 
 
 def abort_if_user_not_found(user_id):
-    user = db_session.create_session().query(User).get(user_id)
-    if not user:
+    if not db_session.create_session().query(User).get(user_id):
         abort(404, message=f"User {user_id} not found")
 
 
 class UsersResource(Resource):
     def get(self, user_id):
-        if not check_api_key():
-            return
         abort_if_user_not_found(user_id)
         return jsonify({'user': db_session.create_session().query(User).get(user_id).to_dict(
             only=('id', 'surname', 'name', 'patronymic', 'age', 'date_of_birth', 'email', 'hashed_password'))})
 
     def delete(self, user_id):
-        if not check_api_key():
-            return
         abort_if_user_not_found(user_id)
         db_sess = db_session.create_session()
         db_sess.delete(db_sess.query(User).get(user_id))
@@ -38,21 +24,21 @@ class UsersResource(Resource):
         return jsonify({'success': 'OK'})
 
     def put(self, user_id):
-        if not check_api_key():
-            return
         args = put_parser.parse_args()
         abort_if_user_not_found(user_id)
         db_sess = db_session.create_session()
         user = db_sess.query(User).get(user_id)
-        if 'password' in args:
+        if args['password']:
             user.set_password(args['password'])
-        if 'surname' in args:
+        if args['surname']:
             user.surname = args['surname']
-        if 'name' in args:
+        if args['name']:
             user.name = args['name']
-        if 'patronymic' in args:
+        if args['patronymic']:
             user.patronymic = args['patronymic']
-        if 'email' in args:
+        if args['email']:
+            if db_sess.query(User).filter(User.email == args['email']).first():
+                return jsonify({'Error': 'A user with this email already exists'})
             user.email = args['email']
         db_sess.commit()
         return jsonify({'success': 'OK'})
@@ -60,20 +46,18 @@ class UsersResource(Resource):
 
 class UsersListResource(Resource):
     def get(self):
-        if not check_api_key():
-            return
         return jsonify({'users': [user.to_dict(
             only=('id', 'surname', 'name', 'patronymic', 'email')) for user
             in db_session.create_session().query(User).all()]})
 
     def post(self):
-        if not check_api_key():
-            return
         args = parser.parse_args()
         db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == args['email']).first():
+            return jsonify({'Error': 'A user with this email already exists'})
         user = User(surname=args['surname'], name=args['name'], patronymic=args['patronymic'], email=args['email'])
         user.set_password(args['password'])
-        user.set_date(args["date_of_birth"])
+        user.set_date(date(*map(int, args["date_of_birth"].split("-"))))
         db_sess.add(user)
         db_sess.commit()
         return jsonify({'success': 'OK'})
@@ -83,7 +67,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('surname', required=True)
 parser.add_argument('name', required=True)
 parser.add_argument('patronymic', required=True)
-parser.add_argument('date_of_birth', required=True, type=date)
+parser.add_argument('date_of_birth', required=True)
 parser.add_argument('email', required=True)
 parser.add_argument('password', required=True)
 put_parser = reqparse.RequestParser()
@@ -92,4 +76,3 @@ put_parser.add_argument('name')
 put_parser.add_argument('patronymic')
 put_parser.add_argument('email')
 put_parser.add_argument('password')
-put_parser.add_argument('key', required=True)
